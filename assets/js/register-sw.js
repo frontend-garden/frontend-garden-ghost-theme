1efc1340985d7e067a6a22071f1da35216824776
Adam Kudrna <adam@adamkudrna.cz>
2020-06-17 10:04:52 +0200

    Temporarily unregister ServiceWorker until page caching issues are resolved

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -1,14 +1,25 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register(
-    '/sw.js',
-  )
-    .then((registration) => {
-      if (registration.waiting) {
-        registration.update();
-      }
-    })
-    .catch((error) => {
-      // eslint-disable-next-line
-      console.error('[Service worker] Oh, no. Registration of SW failed: ', error);
-    });
+  // Adam 2020/06/17:
+  // Temporarily disabled SW until page caching issues are resolved.
+  // navigator.serviceWorker.register(
+  //   '/sw.js',
+  // )
+  //   .then((registration) => {
+  //     if (registration.waiting) {
+  //       registration.update();
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     // eslint-disable-next-line
+  //     console.error('[Service worker] Oh, no. Registration of SW failed: ', error);
+  //   });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations()
+      .then((registrations) => {
+        for (let i = 0; i < registrations.length; i += 1) {
+          registrations[i].unregister();
+        }
+      });
+  }
 }
