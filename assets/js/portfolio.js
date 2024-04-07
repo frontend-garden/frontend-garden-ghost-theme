f575b60dc78bdcb92afcd70cf2576f387759821a
Ramon Guilherme <ramon.nascimento@cs.udf.edu.br>
2024-04-06 22:33:56 -0300

    fix portfolio.js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,3 +1,39 @@
+function filterProjects(tag) {
+  const projectInfosInner = document.querySelectorAll('.project-info');
+  let projectsFound = false;
+  projectInfosInner.forEach((projectInfo) => {
+    const tags = projectInfo.querySelectorAll('.label');
+    let showProject = false;
+    tags.forEach((tagElement) => {
+      if (tagElement.textContent.trim() === tag || tag === 'todos') {
+        showProject = true;
+      }
+    });
+    if (showProject || tag === 'todos') {
+      const projectInfoElement = projectInfo;
+      projectInfoElement.style.display = 'block';
+      projectsFound = true;
+    } else {
+      const projectInfoElement = projectInfo;
+      projectInfoElement.style.display = 'none';
+    }
+  });
+  if (!projectsFound) {
+    const noProjectsMessage = document.querySelector('.no-projects-message');
+    if (!noProjectsMessage) {
+      const newNoProjectsMessage = document.createElement('p');
+      newNoProjectsMessage.classList.add('no-projects-message');
+      newNoProjectsMessage.textContent = 'Não há projetos disponíveis nessa categoria.';
+      document.querySelector('.columns').appendChild(newNoProjectsMessage);
+    }
+  } else {
+    const noProjectsMessage = document.querySelector('.no-projects-message');
+    if (noProjectsMessage) {
+      noProjectsMessage.remove();
+    }
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const projectInfos = document.querySelectorAll('.project-info');
   projectInfos.forEach((projectInfo) => {
@@ -17,41 +53,5 @@ document.addEventListener('DOMContentLoaded', () => {
       filterProjects(tag);
     });
   });
-
-  function filterProjects(tag) {
-    const projectInfosInner = document.querySelectorAll('.project-info');
-    let projectsFound = false;
-    projectInfosInner.forEach((projectInfo) => {
-      const tags = projectInfo.querySelectorAll('.label');
-      let showProject = false;
-      tags.forEach((tagElement) => {
-        if (tagElement.textContent.trim() === tag || tag === 'todos') {
-          showProject = true;
-        }
-      });
-      if (showProject || tag === 'todos') {
-        const projectInfoElement = projectInfo;
-        projectInfoElement.style.display = 'block';
-        projectsFound = true;
-      } else {
-        const projectInfoElement = projectInfo;
-        projectInfoElement.style.display = 'none';
-      }
-    });
-    if (!projectsFound) {
-      const noProjectsMessage = document.querySelector('.no-projects-message');
-      if (!noProjectsMessage) {
-        const newNoProjectsMessage = document.createElement('p');
-        newNoProjectsMessage.classList.add('no-projects-message');
-        newNoProjectsMessage.textContent = 'Não há projetos disponíveis nessa categoria.';
-        document.querySelector('.columns').appendChild(newNoProjectsMessage);
-      }
-    } else {
-      const noProjectsMessage = document.querySelector('.no-projects-message');
-      if (noProjectsMessage) {
-        noProjectsMessage.remove();
-      }
-    }
-  }
 });
   
